37cd6087c940cc9b34aa893971f86aff5c12cbac
xFractu <marioteku99@gmail.com>
2023-10-04 11:48:49 -0600

    practica06

---
+++ practica06.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById("formulario")
+        formulario.addEventListener("submit", evento => {
+            evento.preventDefault()
+        let u = document.getElementById("user").value
+        let p = document.getElementById("password").value
+
+        alert(u + p)
+        })
