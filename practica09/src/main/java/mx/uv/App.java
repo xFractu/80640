4da712670eed1680ea3e9464c0cf20c1eebd48be
xFractu <marioteku99@gmail.com>
2023-10-19 12:55:07 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -4,10 +4,24 @@ package mx.uv;
  * Hello world!
  *
  */
+
+import static spark.Spark.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+        get("/", 
+            (request, response) ->"<h1>Hola Mundo!</h1>"
+            );
+        get("/ruta1", 
+            (request, response) ->"<h1>Adios Mundo!</h1>"
+            );
+        get("/ruta2", 
+            (request, response) ->"<h1>Que tal Mundo!</h1>"
+            );
+        get("/ruta3", 
+            (request, response) ->"{'alumno':'john','matricula':'s12334','carrera':'tc'}"
+            );
     }
 }
