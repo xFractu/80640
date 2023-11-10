978e5d06f56ee09e36709557b8ca6708b4d6587a
xFractu <marioteku99@gmail.com>
2023-11-10 08:30:46 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -11,6 +11,34 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+
+options("/*",(request,response)->{
+
+String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+
+if(accessControlRequestHeaders!=null){
+
+response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+
+}
+
+String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+
+if(accessControlRequestMethod!=null){
+
+response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+
+}
+
+return "OK";
+
+});
+
+before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
+
         get("/", 
             (request, response) ->"<h1>Hola Mundo!</h1>"
             );
@@ -21,7 +49,7 @@ public class App
             (request, response) ->"<h1>Que tal Mundo!</h1>"
             );
         get("/ruta3", 
-            (request, response) ->"{'alumno':'john','matricula':'s12334','carrera':'tc'}"
+            (request, response) ->"{\"alumno\":\"john\",\"matricula\":\"s12334\",\"carrera\":\"tc\"}"
             );
     }
 }
